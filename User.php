1fdad45af059075b801a014ef1843ecb1fe63b11
ALI BENYAHIA Mehdi <mehdi.ali_benyahia@edu.devinci.fr>
2020-10-09 17:19:07 +0200

    first push

---
+++ User.php
@@ -0,0 +1,29 @@
+<?php
+
+require "General.php";
+
+class User extends General {
+
+    protected $table = "user";
+
+    /**
+     * Save user
+     *
+     * @param array $param
+     * @return void
+     */
+    public function save($param){
+        $statement = "INSERT INTO user (nom, prenom, mail, password, secret_api_key)
+                        VALUES (:nom, :prenom :mail, :password, :secret_api_key)";
+        
+        $param["nom"]= htmlspecialchars($param["nom"]);
+        $param["prenom"]= htmlspecialchars($param["prenom"]);
+        $param["mail"]= htmlspecialchars($param["mail"]);
+        $param["password"]= password_hash($param["password"], PASSWORD_DEFAULT);
+        $param["secret_api_key"]= md5(uniqid());
+
+        $this->db->prepare($statement, 'save', $param);
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'App.php';
+$app = new App();
+
+
+
+if (key_exists("page", $_GET)) {
+ $page = ucfirst($_GET["page"]);
+
+  require $page.".php";
+   $class = new $page();
+
+   require 'router.php';
+} else {
+   $app->sendData("Erreur de choix de table");
+}
