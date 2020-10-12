1944fbd8228c3d71a9c7c551ec13a96c1b9e7d15
loks_brt <lucas.beriot@gmail.com>
2020-10-12 23:37:55 +0200

    add

--- User.php
+++ User.php
@@ -12,19 +12,19 @@ class User extends General {
      * @param array $param
      * @return void
      */
-    public function save($param){
-       $statement = "INSERT INTO client (nom, prenom, mail, password)
-                        VALUES (:nom, :prenom :mail, :password)";
+   // public function save($param){
+     //  $statement = "INSERT INTO client (nom, prenom, mail, password, role, secret_api_key)
+    //                    VALUES (:nom, :prenom :mail, :password, :role, :secret_api_key)";
   //
-      $param["nom"]= htmlspecialchars($param["nom"]);
-       $param["prenom"]= htmlspecialchars($param["prenom"]);
-       $param["mail"]= htmlspecialchars($param["mail"]);
-       $param["password"]= password_hash($param["password"], PASSWORD_DEFAULT);
+   //   $param["nom"]= htmlspecialchars($param["nom"]);
+   //    $param["prenom"]= htmlspecialchars($param["prenom"]);
+    //   $param["mail"]= htmlspecialchars($param["mail"]);
+    //   $param["password"]= password_hash($param["password"], PASSWORD_DEFAULT);
     //      $param["role"]= json_encode(["ROLE_USER"]);
     //      $param["secret_api_key"]= md5(uniqid());
 //
-        $this->db->prepare($statement, 'save', $param);
-    }
+     //   $this->db->prepare($statement, 'save', $param);
+   // }
 
 
 
